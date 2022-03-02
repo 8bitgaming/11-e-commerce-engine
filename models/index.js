52a91b77b5c7a6845b549b5dc3a53260ecfbbe7b
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-03-01 20:38:51 -0600

    updated index model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Post, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'tag_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Post, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
